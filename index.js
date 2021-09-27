6d2df7e80407f9953fdf9273c36cff99109bf239
Eric <eric.yanez50@qmail.cuny.edu>
2021-09-26 20:31:34 -0400

    commit tree 0652cc5f0de41d2644f4f6e69a3f4026a5f83f8f

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let multiply
+let num1 = 31
+let num2 = 2
+multiply = num1 * num2
+
+let random
+random = Math.floor(Math.random( )* 10)
+
+let mod
+let num3 = 12
+let num4 = 3
+mod = num3 / num4
+
+let max
+max = Math.max(0,20)
 
 // Write your code here
